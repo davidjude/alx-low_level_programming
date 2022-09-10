a0edb9aa1d9ed9ee067132bb55d8f0b5d02be0a8
davejude <dagwu79@gmail.com>
2022-09-09 20:00:31 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: 0 success
- */
+* main - Entry point
+*
+* Return: 0 success
+*/
 int main(void)
 
 {
 char base_16;
 
-for (base_16 = 48; base_16 <= 57; base 16++)
-{putchar(base_16);
+for (base_16 = 48; base_16 <= 57; base_16++)
+{
+putchar(base_16);
 }
 
 for (base_16 = 97; base_16 <= 102; base_16++)
