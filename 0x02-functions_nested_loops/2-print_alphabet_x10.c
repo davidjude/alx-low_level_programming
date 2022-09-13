bbdac6e7554e9f40affaf1138195650ffae21180
davejude <dagwu79@gmail.com>
2022-09-13 15:49:45 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - Describes the main function
+*
+*Description:
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+		_putchar('\n');
+
+	}
+}
+
