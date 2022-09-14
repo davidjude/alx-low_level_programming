08a8999ce45f4a0c86179ec19790b4a83eeefe46
davejude <dagwu79@gmail.com>
2022-09-14 06:04:11 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Describes the main function
+ *
+ * Description:
+ * @a: some int
+ * @b: some int
+ * Return: int;
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
