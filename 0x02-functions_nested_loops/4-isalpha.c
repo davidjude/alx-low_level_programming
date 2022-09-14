e4f168f34aa3fa35a8c6a9fa08baad38ed68e946
davejude <dagwu79@gmail.com>
2022-09-14 05:36:44 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - Describes the main function
+ *
+ * Description:
+ * @c: is the parameter
+ * Return: int;
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
